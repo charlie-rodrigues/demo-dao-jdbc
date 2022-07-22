e2b15c56b5827ba6af2fa0320790b4709f9eb0d5
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-07-22 17:23:45 -0300

    create interfaces Department and Seller DAO

---
+++ src/model/dao/DepertmentDAO.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepertmentDAO {
+void insert(Department obj);
+void update(Department obj);
+void delete(Department obj);
+Department findById(Integer id);
+List<Department>findAll();
+
+}
