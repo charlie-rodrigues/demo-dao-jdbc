9dacbc21b438d89a3e9ffa726e3b08a332a1e2ac
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-07-22 18:09:51 -0300

    seller dao and dao factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,7 @@ public class Program {
 Department obj = new Department(2, "books");
 Seller seller = new Seller(1, "rulio", "rulio@gmail.com", new Date(), 3000.0, obj);
 System.out.println(seller);
+SellerDAO dao = DaoFactory.createSellerDao();
 	}
 
 }
--- src/model/dao/SellerDAO.java
+++ src/model/dao/SellerDAO.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface SellerDAO {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDAO {
+void insert(Seller obj);
+void update(Seller obj);
+void delete(Seller obj);
+Seller  findById(Integer id);
+List<Seller>findAll();
 }
