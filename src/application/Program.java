de1d5e409f0a08ac4d171f7b32478d196eaced57
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-07-22 17:07:12 -0300

    create entity seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 Department obj = new Department(2, "books");
-System.out.println(obj);
+Seller seller = new Seller(1, "rulio", "rulio@gmail.com", new Date(), 3000.0, obj);
+System.out.println(seller);
 	}
 
 }
